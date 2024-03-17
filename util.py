81046f44951cee1e8b7da43993ff0734b8c2cdd1
davnish <nischal.singh38@gmail.com>
2024-03-17 10:40:43 +0530

    fps added

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-from dataset import Dales
+from dataset import Dales, modelnet40
 import open3d as o3d
 import numpy as np
 import torch
@@ -14,29 +14,59 @@ def cal_mx_distance(pnt, data):
     return data[torch.argmax(dist)]
 
 
-def fps(data):
-    pnts = []
-    pnts.append(data[0])
-    while len(pnts) != num_points:
-        pnts.append(cal_mx_distance(pnts[-1], data))
+def fps(points, num_points):
+    points = np.asarray(points)
+    sampled_pnts = np.zeros(num_points, dtype='int') # Sampled points
+    pnts_left = np.arange(len(points), dtype='int') # points not sampled 
+    dist = np.ones_like(pnts_left) * float('inf') # dist array
 
-    return pnts
+    selected = 0 # Selected current point
+    sampled_pnts[0] = selected
+    pnts_left = np.delete(pnts_left, selected)
+    # dist = np.linalg.norm(points[pnts_left] - points[selected], ord = 2)
+
+
+    for i in range(1, num_points):
+        
+
+        selected_dist = np.linalg.norm(points[pnts_left] - points[selected], ord = 2)
+
+        # temp = np.linalg.norm(points[pnts_left] - points[selected], ord = 2)
+
+        dist[pnts_left] = np.minimum(dist[pnts_left], selected_dist)
+        # print(dist)
+        selected = np.argmax(dist[pnts_left], axis = 0)
+        # print(selected)
+        sampled_pnts[i] = pnts_left[selected]
+
+        pnts_left = np.delete(pnts_left, selected)
+
+    return points[sampled_pnts]
+
 
 if __name__ == "__main__":
-    # train = Dales(25, 2048)
+    train = modelnet40()
     num_points = 10
+    print(train[0][0])
+    # train = fps(train[0][0], 2048)
+    train = train[0][0]
+    print(train)
+    # a = np.arange(10)
+    # print(a)
+    # a = np.delete(a, 2)
+    # print(a)
     # train = fps(train[0][0])
     # print(len(train))
     # # print(train[0][0][0])
 
     # print(train)
-    # pcd = o3d.geometry.PointCloud()
-    # pcd.points = o3d.utility.Vector3dVector(train)
-    torch.manual_seed(42)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(train)
+    # torch.manual_seed(42)
 
-    a = torch.randint(10, (5,1))
-    b = torch.randint(10, (5,1))
+    # a = torch.randint(10, (5,1))
+    # b = torch.randint(10, (5,1))
 
-    c = np.minimum(a,b)
-    print(a, b, c)
-    # o3d.visualization.draw_geometries([pcd])
+    # c = np.minimum(a,b)
+    # print(a, b, c)
+    o3d.visualization.draw_geometries([pcd])
